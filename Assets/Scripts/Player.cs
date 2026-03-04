828c07b553139a406d9720c397153fe52e48ac5b
Netch Dev <40652014+netch-dev@users.noreply.github.com>
2026-03-03 23:04:31 -0800

    Add simple player movement using the legacy input system

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	[SerializeField] private float speed = 5f;
+
+	private void Update() {
+		Vector2 inputVector = new Vector2(0, 0);
+
+		if (Input.GetKey(KeyCode.W)) { 
+			inputVector.y += 1;
+		}
+
+		if (Input.GetKey(KeyCode.S)) {
+			inputVector.y -= 1;
+		}
+
+		if (Input.GetKey(KeyCode.A)) {
+			inputVector.x -= 1;
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			inputVector.x += 1;
+		}
+
+		// Normalize the input vector to ensure consistent movement speed in all directions
+		inputVector = inputVector.normalized;
+		Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * speed;
+		transform.position += moveDir ;
+	}
+}
