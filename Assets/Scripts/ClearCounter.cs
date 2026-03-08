59805c3adc901a685cb8642bd138738b80008417
Netch Dev <40652014+netch-dev@users.noreply.github.com>
2026-03-08 03:07:37 -0700

    Link KitchenObject and ClearCounter
    Make ClearCounter track a KitchenObject and add API to set/get/clear it.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 
+// TODO: Implement an IInteractable interface
 public class ClearCounter : MonoBehaviour {
-	// TODO: Implement an IInteractable interface
-
 	[SerializeField] private Transform counterTopPoint;
 	[SerializeField] private KitchenObjectSO tomatoSO;
-	public void Interact() {
-		Transform tomato = Instantiate(tomatoSO.prefab, counterTopPoint);
-		tomato.transform.localPosition = Vector3.zero;
 
-		KitchenObjectSO so = tomato.transform.GetComponent<KitchenObject>().GetKitchenObjectSO();
-		Debug.Log(so.objectName);
+	private KitchenObject kitchenObject;
+	public void Interact() {
+		if (kitchenObject == null) {
+			Transform tomato = Instantiate(tomatoSO.prefab, counterTopPoint);
+			tomato.GetComponent<KitchenObject>().SetClearCounter(this);
+		}
+	}
+
+	public Transform GetCounterTopPoint() {
+		return counterTopPoint;
+	}
+
+	public void SetKitchenObject(KitchenObject kitchenObject) {
+		this.kitchenObject = kitchenObject;
+	}
+
+	public KitchenObject GetKitchenObject() {
+		return kitchenObject;
+	}
+
+	public void ClearKitchenObject() {
+		kitchenObject = null;
+	}
+
+	public bool HasKitchenObject() {
+		return kitchenObject != null;
 	}
 }
